9c4cf23e6d5bcba3cb222a133b237e5d7d9c9b52
vivacious-revert-7013 <mesmith454@gmail.com>
2020-10-27 16:05:17 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+  sum = num1 + num2
+  p sum
 end
 
 def subtraction(num1, num2)
-
+  dif = num1 - num2
+  p dif
 end
 
 def division(num1, num2)
-
+  divi = num1 / num2
+  p divi
 end
 
 def multiplication(num1, num2)
-
+  prod = num1 * num2
+  p prod
 end
 
 def modulo(num1, num2)
-
+  percent = num1 % num2
+  p percent
 end
 
 def square_root(num)
-
+  square = Math.sqrt(num)
+  p square
 end
